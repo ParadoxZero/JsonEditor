6032e17874c20d4e95722da2e110ac72e7e97df9
Sidhin S Thomas <thomas.sidhin@outlook.com>
2024-09-07 17:59:22 +0530

    Add app controller

--- App.xaml.cs
+++ App.xaml.cs
@@ -41,10 +41,8 @@ namespace WebInfoEditor
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
-            m_window.Activate();
+            AppController.GetInstance().OnAppStart();
         }
 
-        private Window m_window;
     }
 }
---
+++ AppController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace WebInfoEditor
+{
+    internal class AppController
+    {
+        private static AppController _instance;
+        private MainWindow window;
+
+        public static AppController GetInstance()
+        {
+            _instance ??= new AppController();
+            return _instance;
+        }
+
+        private AppController() { }
+
+        public void OnAppStart()
+        {
+            window = new MainWindow();
+            window.Activate();
+        }
+    }
+
+}
